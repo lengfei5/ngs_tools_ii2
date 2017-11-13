6cebe5a2a6c61bab2a83afed225a8cdb20c37b23
jingkui <jingkui.wang@imp.ac.at>
2017-11-13 16:40:32 +0100

    done the first optimization for get_NGS.sh : using the text file of vbcf links to download and convert bam to fastq, fastqc control

--- get_NGS.sh
+++ get_NGS.sh
@@ -1,48 +1,65 @@
-#####
+#######
 # this script is to down unaligned bam files using links sent from vbcf
 # convert bam to fastq
-####
+######
 # print host name and date
 hostname
 date
 
 file_urls="URLs_download.txt"
-nb_cores=1
+nb_cores=2
 
 cwd=`pwd`
 DIR_BAMs=$PWD/ngs_raw/BAMs
 DIR_FASTQs=$PWD/ngs_raw/FASTQs
 mkdir -p $DIR_BAMs;
+mkdir -p $DIR_FASTQs;
 mkdir -p $cwd/logs;
 
 # download bam files from links
-while read -r line; do
-    #echo $line;
+while IF=$'\t' read -r line; do
+    echo $line;
     IFS=$'\t' read -r "id" "type" "flowcell" "lane" "result" "countsQ30" "distinct" "preparation" "own_risk" "url" "md5" "comment" <<<  "$line"
+    #echo $line{@}
+    #url=`echo $line |tr`
     #echo $line;
-    #echo $url
+    echo $url
     #echo $own_risk
     #echo $md5
     #exit;
     ff=`basename $url`
-    #echo $ff;
+    ff=${url##*/}
+    echo $ff;
     
     if [ ! -e "${DIR_BAMs}/$ff" ] && [ $url=="http:*" ]; then
-	echo $url
+	#echo $url
 	#echo $url2
 	# download
-	wget -c --no-check-certificate --auth-no-challenge --user 'Jingkui.Wang' --password "password" $url -P $DIR_BAMs
-	# bamtofastq
-	ff=`basename $file`
-        #if [ -e "${DIR_BAMs}/$ff" ]; then
-	out=${ff%.bam};
-	echo $out
-	qsub -q public.q -o ${cwd}/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N star_mapping " module load bedtools; bamToFastq -i ${DIR_BAMs}/$ff -fq ${DIR_FASTQs}/${out}.fastq; module load fastqc; fastqc ${DIR_FASTQs}/${out}.fastq -o ${DIR_QC}; module load star/2.5.0a; STAR --runThreadN $nb_cores --genomeDir $GENOME --readFilesIn ${DIR_FASTQs}/${out}.fastq --outFileNamePrefix ${DIR_alignment}/$out --outSAMtype BAM SortedByCoordinate --outWigType wiggle --outWigNorm RPM; "
-	
+	wget -c --no-check-certificate --auth-no-challenge $url -P $DIR_BAMs
+			
     else
 	echo "$ff already downloaded ! "
     fi
+    
 done < "$file_urls"
+
+## convert bam to fastq
+if [ `ls -l $DIR_BAMs/*.bam 2>/dev/null|wc -l` -gt 0 ]; then
+    for file in $DIR_BAMs/*.bam;
+    do 
+	ff=`basename $file`
+        out=${ff%.bam};
+	echo $out
+	if [ "${DIR_FASTQs}/${out}.fastq" -ot "$file" ]; then
+	    qsub -q public.q -o ${cwd}/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N star_mapping " module load bedtools; bamToFastq -i ${DIR_BAMs}/$ff -fq ${DIR_FASTQs}/${out}.fastq; module load fastqc; fastqc ${DIR_FASTQs}/${out}.fastq -o ${DIR_QC};"
+	
+	fi
+    done
+else
+    echo "No Bam files"
+fi
+
+
 
 
 
