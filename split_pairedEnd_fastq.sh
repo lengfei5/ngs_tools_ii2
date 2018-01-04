1df4d614231e040061776ade1c7b2eb87a8389a2
jingkui <jingkui.wang@imp.ac.at>
2018-01-04 11:18:32 +0100

    add scripts to split paired_end fastq files

---
+++ split_pairedEnd_fastq.sh
@@ -0,0 +1,26 @@
+###############
+## this script is to split paired_end fastq file into forward and reverse fastq files
+## by involking another function deinterleave_fastq.sh
+###############
+nb_cores=2
+cwd=`pwd`
+deinterleaver="/home/imp/jingkui.wang/scripts/ngs_tools/deinterleave_fastq.sh"
+
+#fastqs=$1
+#echo 
+#DIR_input="${cwd}/ngs_raw/FASTQs_toTrimmed"
+#DIR_trimmed="${cwd}/ngs_raw/FASTQs"
+#echo $DIR_input
+#echo $DIR_trimmed
+
+mkdir -p "${cwd}/logs"
+#mkdir -p $DIR_trimmed
+
+for file in "$@" 
+do
+    echo $file
+    split_fastq="${file%.fastq}"
+    #echo $trimmed
+    qsub -q public.q -o ${cwd}/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N splitFastq "bash ${deinterleaver} < ${file} ${split_fastq}_R1.fastq ${split_fastq}_R2.fastq; "
+
+done
