49e07a4f995c91e23e5d255106dfa4f63e2fb6a6
jingkui <jingkui.wang@imp.ac.at>
2018-01-04 14:10:28 +0100

    add run_fastqc.sh

---
+++ run_fastqc.sh
@@ -0,0 +1,23 @@
+############
+## this script is to run fastqc for the ngs data 
+############
+nb_cores=2
+cwd=$PWD
+
+DIR_fastq="${PWD}/ngs_raw/FASTQs"
+#echo $DIR_fastq
+DIR_FastQCs=$PWD/ngs_raw/FastQCs;
+
+mkdir -p $PWD/logs
+mkdir -p ${DIR_FastQCs} 
+
+## run fastQC for quality control
+for file in ${DIR_fastq}/*.fastq;
+do
+    echo $file
+    index_name="$(basename $file)"                                                                                                                                                                                                                        
+    fname=${index_name%.bam}                                                                                                                                                                                                                               
+    #echo $fname                                                                                                                                                                                                                                        
+    qsub -q public.q -o $cwd/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N fastqc "module load fastqc; fastqc $file -o ${DIR_FastQCs} "
+done
+
