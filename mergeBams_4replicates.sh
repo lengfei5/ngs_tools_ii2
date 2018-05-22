73a6faf02a48d508607f02d95067187809e8a7ec
jingkui <jingkui.wang@imp.ac.at>
2018-05-22 13:26:47 +0200

    rewrite the script mergeBams_4replicates.sh by using design matrix as input and adding technical and biological replicates as new options

--- mergeBams_4replicates.sh
+++ mergeBams_4replicates.sh
@@ -6,17 +6,19 @@
 # a text file that specifies according what to merge 
 # (same sample ID or replicates, i.e., same condition) and file names after merging. 
 ############
-while getopts ":hD:O:f:" opts; do
+while getopts ":hD:O:f:tr" opts; do
     case "$opts" in
         "h")
             echo "This script is to merge bam files requiring arguments: "
-	    echo "input (directory for bams files) "
-            echo "output directory (optional if different from the directory of initial bams) "
-            echo "a text file that specifies according what to merge "
-            echo "(same sample ID or replicates, i.e., same condition) and file names after merging." 
+	    echo "-D (input directory for bams files) "
+            echo "-O (output directory optional if different from the directory of initial bams) "
+            echo "-f (a text file for the sample infos in the form of design matrix: \
+                      two columns, sampleID and Conditions)"
+            echo "-t (to merge technical replicates with same sample ID)" 
+	    echo "-r (to merge biological replicates, i.e., same condition)." 
 	    echo "....................";
             echo "Usage:"
-            echo "$0 -D alignments/BAMs_All -O BAMs_merged -f file4merged.txt"
+            echo "$0 -D alignments/BAMs_All -O BAMs_merged -f design_matrix.txt -t (or -r)"
             exit 0
             ;;
         "D")
@@ -27,6 +29,12 @@ while getopts ":hD:O:f:" opts; do
 	    ;;
 	"f")
 	    params="$OPTARG";
+	    ;;
+	"t")
+	    technicalRep="TRUE"
+	    ;;
+	"r")
+	    biologicalRep="TRUE"
 	    ;;
         "?")
             echo "Unknown option $opts"
@@ -41,60 +49,79 @@ done
 if [ -z "$DIR_OUT" ]; then 
     DIR_OUT=$DIR_Bams;
 fi
+merge_techRep="FALSE";
+merge_bioRep="FALSE";
+if [ "$technicalRep" == "TRUE" ]; then
+    if [ -z "$biologicalRep" ]; then
+	merge_techRep="TRUE";
+    else
+	echo "make a choice -- to merge technical or biological replicates"
+	exit 1;
+    fi
+fi    
+if [ "$biologicalRep" == "TRUE" ]; then
+    if [ -z "$technicalRep" ]; then
+	merge_bioRep="TRUE";
+    else
+	echo "make a choice -- to merge technical or biological replicates"
+	exit 1;
+    fi
+fi
 
 cwd=$PWD;
-nb_cores=2;
+nb_cores=1;
 
+DIR_backup=${DIR_OUT}/before_merging
 mkdir -p $DIR_OUT
+mkdir -p $DIR_backup
 mkdir -p $cwd/logs
 
-#i=1;
-#cd $DIR_Bams;
-#for ss in $factors; 
-#red=`tput setaf 1`
-while read -r line; do   
-    #echo $line;
-    IFS=$'\t' read -r "id" "cond" <<< "$line"
+# loop over the design matrix file
+if [ "$merge_techRep" == "TRUE" ]; then
+    echo "-- Merge Technical Replicates --"
+    tomerge=(`cat $params | cut -f1 | sort -u |grep -v sampleID`)
+    #echo $tomerge
+else
+    echo "-- Merge Biological Replicates --"
+    tomerge=(`cat $params | cut -f2 | sort -u |grep -v fileName|grep -v condition`)
+fi
+#echo $tomerge;
+
+#exit;
+for selection in "${tomerge[@]}"; do
+    #echo $selection
+    #exit
+    # find the bam to merge
+    old=($(ls ${DIR_Bams}/*.bam | grep "$selection"));
     
-    if [ "$id" != "sampleID" ]; then
-	old=($(ls ${DIR_Bams}/*.bam | grep "$id"));
-	
-	echo ${#old[@]} "Files :  " $id "--" $cond
-	#echo $cond
-	#echo $bams
-	#echo $old;
-	#echo ${old[@]};
-	
-	out=${cond}_${id}_merged
-	
-	if [[ ${#old[@]} -gt 1 ]]; then
-	    	    
-	    #out=${out}_merged;
-	    #echo $out;
-	    #echo here
-	    if [ ! -e "${DIR_OUT}/${out}.bam" ]; then
-		#echo "here"
-		qsub -q public.q -o $cwd/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N mergeBam "module load samtools/1.3.1; samtools merge ${DIR_OUT}/${out}_unsorted.bam ${old[@]}; samtools sort -o $DIR_OUT/${out}.bam $DIR_OUT/${out}_unsorted.bam; samtools index ${DIR_OUT}/${out}.bam; rm $DIR_OUT/${out}_unsorted.bam; "
-	    fi;
-	
-	elif [[ ${#old[@]} -eq 1 ]]; then
-	    #echo ${#old[@]};
-	    echo "---------------WARNING: ONLY ONE file found--------- "
-	    #echo "why is here"
-	    #out=${}_merged
-	    #ids=`echo "$old" | cut -d'_' -f3`
-	    #out=${cond}${ss}_${ids}_merged
-	    #echo ${old[@]};
-	    #echo $ids
-	    #echo $out;
-	    if [ ! -e "${DIR_OUT}/${out}.bam" ]; then
-		qsub -q public.q -o $cwd/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N mergeBam "cp ${old[@]} ${DIR_OUT}/${out}.bam; module load samtools/1.3.1; samtools index ${DIR_OUT}/${out}.bam; "
-	    fi;
+    # the name for merged file
+    if [ "$merge_techRep" == "TRUE" ]; then
+	cond=`cat $params | grep $selection |cut -f2|sort -u`
+	if [[ ${#cond[@]} -eq 1 ]]; then
+	    out=${cond}_${selection}
 	else
-	    echo "NOT FOUND Bam files for $ss and $cond "
+	    echo "ERROR-- > 1 conditions found !!! "
 	fi
-	#echo ">>>>>"
-	#break;
+    else
+	id=`cat $params|grep $selection |cut -f1|sort -u| tr '\n' '.'`
+	out=${selection}_${id}merged
     fi
-done < "$params" 
+    
+    if [[ ${#old[@]} -gt 1 ]]; then
+	if [ ! -e "${DIR_OUT}/${out}.bam" ] ; then
+	    echo ${#old[@]} "Files for --" $selection  "-- with file name --" $out
+	    qsub -q public.q -o $cwd/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N mergeBam "module load samtools/1.3.1; samtools merge ${DIR_OUT}/${out}_unsorted.bam ${old[@]}; samtools sort -o $DIR_OUT/${out}.bam $DIR_OUT/${out}_unsorted.bam; samtools index ${DIR_OUT}/${out}.bam; rm $DIR_OUT/${out}_unsorted.bam; mv ${old[@]} $DIR_backup;"
+	fi;
+    fi
+    
+    if [[ ${#old[@]} -eq 1 ]] && [ "$merge_bioRep" == "TRUE" ]; then
+	echo ${#old[@]} "Files for --" $selection  "-- with file name --" $out
+   	if [ ! -e "${DIR_OUT}/${out}.bam" ]; then
+	    echo qsub -q public.q -o $cwd/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N mergeBam "cp ${old[@]} ${DIR_OUT}/${out}.bam; module load samtools/1.3.1; samtools index ${DIR_OUT}/${out}.bam; mv ${old[@]} $DIR_backup;"
+	fi;
+    fi
+    
+    #break;
+done
+
 
