dedcce009366fc181a57d9dff615d8f2840f5976
jingkui.wang <jingkui.wang@imp.ac.at>
2018-10-30 11:55:47 +0100

    update mainly for bam2fastq.sh which has been modified for slurm

--- bam2fastq.sh
+++ bam2fastq.sh
@@ -1,7 +1,6 @@
 ###################
 ## bam2fastq script
 ###################
-nb_cpus=2
 DIR_INPUT="${PWD}/ngs_raw/BAMs"
 DIR_OUT="${PWD}/ngs_raw/FASTQs"
 echo ${DIR_OUT};
@@ -12,12 +11,34 @@ mkdir -p $PWD/logs
 for file in $DIR_INPUT/*.bam;
 do 
     echo "$file"
-    #echo "$(dirname $file)"
-    #echo "$(basename $file)"
     FILENAME="$(basename $file)";
     fname=${FILENAME%.bam};
     echo $fname
-    qsub -q public.q -o $PWD/logs -j yes -N bam2fastq -cwd -b y -pe smp $nb_cpus -shell y "module load bedtools; \
-    bamToFastq -i $file -fq ${DIR_OUT}/${fname}.fastq;" 
+    
+    ## creat the script for each sample 
+    script=$PWD/logs/$fname.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
+
+
+#SBATCH --cpus-per-task=1 
+#SBATCH --time=60 
+#SBATCH --mem=10000 
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o $PWD/logs/$fname.out 
+#SBATCH -e $PWD/logs/$fname.err 
+#SBATCH --job-name bam2fq
+
+module load bedtools/2.25.0-foss-2017a;
+
+bamToFastq -i $file -fq ${DIR_OUT}/${fname}.fastq;
+
+EOF
+    
+    cat $script;
+    sbatch $script
     #break;
+    
 done
