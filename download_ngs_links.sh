e69edd573deadf1203a410c2eebd785a4c398548
jingkui.wang <jingkui.wang@imp.ac.at>
2019-02-11 15:45:43 +0100

    update the download_ngs_links.sh

--- download_ngs_links.sh
+++ download_ngs_links.sh
@@ -1,6 +1,8 @@
 #########################################
 # download ngs data using URL links
-#
+# input: text file with url download links
+# output: downloaed bam files in the current directory
+# not yet updated for the ii2 cluster
 #########################################
 file_urls="$1";
 nb_cores=1;
@@ -18,8 +20,12 @@ while read -r line; do
 	    echo $url
 	    echo $file
 	    echo $ext
-	    qsub -q public.q -o $DIR_cwd/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N download "wget --retry-connrefused -t 0 -c --no-check-certificate --auth-no-challenge $url; touch $file; if [ '$ext' == 'gz' ]; then gunzip $file; fi "
-
+	    #qsub -q public.q -o $DIR_cwd/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N download "wget --retry-connrefused -t 0 -c --no-check-certificate --auth-no-challenge $url; touch $file; if [ '$ext' == 'gz' ]; then gunzip $file; fi "
+	    wget --retry-connrefused -t 0 -c --no-check-certificate --auth-no-challenge $url; 
+	    touch $file; 
+	    if [ '$ext' == 'gz' ]; then 
+		gunzip $file; 
+	    fi
 	else
 	    echo "$file -- downloaded !!!"
 	fi
