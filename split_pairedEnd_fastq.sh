2d70c0cc23ad05000681194dc76218f5663dfe84
jingkui.wang <jingkui.wang@imp.ac.at>
2019-12-10 16:32:00 +0100

    save the old version for ii1 and adapted for cbe

--- split_pairedEnd_fastq.sh
+++ split_pairedEnd_fastq.sh
@@ -1,26 +1,41 @@
 ###############
 ## this script is to split paired_end fastq file into forward and reverse fastq files
 ## by involking another function deinterleave_fastq.sh
+## now old version for ii1 is saved in another script
+## this one is adapted for CBE
 ###############
-nb_cores=6
-cwd=`pwd`
-deinterleaver="/home/imp/jingkui.wang/scripts/ngs_tools/deinterleave_fastq.sh"
+DIR=`pwd`
+deinterleaver="~/scripts/ngs_tools/deinterleave_fastq.sh"
 
-#fastqs=$1
-#echo 
-#DIR_input="${cwd}/ngs_raw/FASTQs_toTrimmed"
-#DIR_trimmed="${cwd}/ngs_raw/FASTQs"
-#echo $DIR_input
-#echo $DIR_trimmed
-
-mkdir -p "${cwd}/logs"
+mkdir -p "${DIR}/logs"
 #mkdir -p $DIR_trimmed
 
-for file in "$@" 
+jobName='splitPE'
+for file in "$@"
 do
     echo $file
-    split_fastq="${file%.fastq}"
+    fname="${file%.fastq}"
     #echo $trimmed
-    qsub -q public.q -o ${cwd}/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N splitFastq "bash ${deinterleaver} < ${file} ${split_fastq}_R1.fastq ${split_fastq}_R2.fastq; "
 
+    # creat the script for each sample
+    script=$PWD/${fname}_${jobName}.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
+
+#SBATCH --cpus-per-task=1
+#SBATCH --time=120
+#SBATCH --mem=8000
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o $DIR/logs/$fname.out
+#SBATCH -e $DIR/logs/$fname.err
+#SBATCH --job-name $jobName
+
+bash ${deinterleaver} < ${file} ${fname}_R1.fastq ${fname}_R2.fastq
+
+EOF
+
+    cat $script;
+    sbatch $script
+    
 done
