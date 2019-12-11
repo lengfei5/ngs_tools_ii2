f32e313cf92466279ca5126f4cd6eaddbe55e046
jingkui.wang <jingkui.wang@imp.ac.at>
2019-12-11 15:44:34 +0100

    done configuration for cbe and add messages

--- parsing_specialCharacters_4designMatrix.R
+++ parsing_specialCharacters_4designMatrix.R
@@ -1,97 +1,65 @@
-##################################################
-## Project: General purpose 
-## Script purpose: convert excel sheet (e.g. .csv, .xlsx) to a text file 
-## Usage example : Rscript convertExcel2txt.R example.xlxs 
-## Author: Jingkui Wang (jingkui.wang@imp.ac.at)
-## Date of creation: Mon Dec  4 10:26:20 2017
-##################################################
-args = commandArgs(trailingOnly=TRUE)
-
-# args = "R5750_Sample_IDs.xlsx"
-# test if there is at least one argument: if not, return an error
-if (length(args)==0){
-  stop("At least one argument must be supplied (input_file).xlsx, .csv or .txt", call.=FALSE)
-}else{
-  strparsing = function(x)
-  {
-    #x = sheet[2, 2]
-    y = as.character(x);
-    patterns = c("-",  "/", " ","+", "//")
-    strs = unlist(strsplit(y, ""))
-    mm = match(strs, patterns)
-    strs[which(!is.na(mm)==TRUE)] = "."
-
-    jj2remove = c()
-    for(n in 1:length(strs) )
-    {
-      if(n == 1 | n == length(strs))
-      {
-         if(strs[n] == ".") jj2remove = c(jj2remove, n);
-         
-      }else{
-        if(strs[n-1] == "." & strs[n] == ".") jj2remove = c(jj2remove, n);
-      }
-            
-    }
-
-    if(length(jj2remove)>0)
-      strs = strs[-jj2remove];
-    
-    strs = paste0(strs, collapse = "")
-    #strs = gsub("....", ".", strs);
-    #strs = gsub("...", ".", strs);
-    #strs = gsub("..", ".", strs);
-    return(strs)
-  }
-  
-  library(openxlsx)
-  library(tools)
-  for(n in 1:length(args))
-  {
-    filename = file_path_sans_ext(args[n])
-    extension = file_ext(args[n])
-    if(extension == "xlsx"){
-      sheet = read.xlsx(args[n], sheet = 1, colNames = TRUE,  rowNames = FALSE)
-    }else{
-      if(extension == "csv"){
-        sheet = read.csv(args[n], header = TRUE)
-      }else{
-        if(extension == "txt"){
-          sheet = read.delim(args[n], header = TRUE, sep = '\t')
-        }else{
-          stop("Input file must be .csv or .xlsx or .txt", call. = FALSE)
-        }
-      }
-    }
-    
-    if(ncol(sheet)>2){
-
-      newff = data.frame(sheet[ ,1], apply(sheet[, -1], 1, function(x) paste0(x, collapse = "_")))
-      newff = data.frame(newff[ ,1], sapply(newff[, 2], strparsing))
-
-    }else{
-      if(ncol(sheet)==2){
-
-        newff = data.frame(sheet[, 1], sapply(sheet[, -1], strparsing))
-      }else{
-        cat("ERROR: there is ONLY one column....")
-      }
-      
-    }
-    
-    colnames(newff) = c("sampleID", "fileName")
-    write.table(newff, file=paste0(filename, "_parsed.txt"),
-                sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
-    #print(sheet[1,])
-    
-    #if(extension == "txt"){
-    #  sheet = read.xlsx(args[n], sheet = 1, colNames = TRUE,  rowNames = FALSE)
-    #}
-    
-    #if (length(args)==1){
-    # default output file
-    #args[2] = "out.txt"
-    #}
-    
-  }
-}
+Script started on 2019-12-11 15:39:00+01:00
+^C^C
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m ^C
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m ^C
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m emacs README.shll[Kqstat[3Pllemacs README.sh[Kemacs README.shll[Kqstat[3Pllemacs README.sh[Kemacs README.sh[KRscript ~/scripts/ngs_tools/parsing_specialCharacters_4designMatrix.R s ampleInfos.xlsx
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m ll
+total 310
+drwxrwx---. 3 jingkui.wang cochella.grp   136 Dec 10 16:27 [0m[01;34molds[0m
+-rwxrwxr-x. 1 jingkui.wang cochella.grp  2001 Dec 10 16:40 [01;32mREADME.sh~[0m
+drwxrwx---. 8 jingkui.wang cochella.grp   159 Dec 10 17:07 [01;34mngs_raw[0m
+drwxrwx---. 2 jingkui.wang cochella.grp  5696 Dec 10 17:10 [01;34mlogs[0m
+drwxrwx---. 3 jingkui.wang cochella.grp    26 Dec 11 14:48 [01;34malignments[0m
+-rwxrwx---. 1 jingkui.wang cochella.grp 10284 Dec 11 15:00 [01;32mR8764.xlsx[0m
+-rwxrwx---. 1 jingkui.wang cochella.grp  8890 Dec 11 15:08 [01;32msampleInfos.xlsx[0m
+-rwxrwxr-x. 1 jingkui.wang cochella.grp  2002 Dec 11 15:37 [01;32mREADME.sh[0m
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m less l[KRscript ~/scripts/ngs_tools/parsing_specialCharacters_4designMatrix.R saampleInfos.xlsx[K[K[K[K[K[K[K[K[K[K[K[K[K[K[K[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[K
+[K[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[K   [A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[K
+[K[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cs ma[K[KampleInfos.xlsx 
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m Rscript ~/scripts/ngs_tools/parsing_specialCharacters_4designMatrix.R saampleInfos.xlsx 
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m ll
+total 312
+drwxrwx---. 3 jingkui.wang cochella.grp   136 Dec 10 16:27 [0m[01;34molds[0m
+-rwxrwxr-x. 1 jingkui.wang cochella.grp  2001 Dec 10 16:40 [01;32mREADME.sh~[0m
+drwxrwx---. 8 jingkui.wang cochella.grp   159 Dec 10 17:07 [01;34mngs_raw[0m
+drwxrwx---. 2 jingkui.wang cochella.grp  5696 Dec 10 17:10 [01;34mlogs[0m
+drwxrwx---. 3 jingkui.wang cochella.grp    26 Dec 11 14:48 [01;34malignments[0m
+-rwxrwx---. 1 jingkui.wang cochella.grp 10284 Dec 11 15:00 [01;32mR8764.xlsx[0m
+-rwxrwx---. 1 jingkui.wang cochella.grp  8890 Dec 11 15:08 [01;32msampleInfos.xlsx[0m
+-rwxrwxr-x. 1 jingkui.wang cochella.grp  2002 Dec 11 15:37 [01;32mREADME.sh[0m
+-rwxrwx---. 1 jingkui.wang cochella.grp   410 Dec 11 15:41 [01;32msampleInfos_parsed.txt[0m
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m less sampleInfos_parsed.txt 
+7[?47h[?1h=sampleID        fileName
+103052  MLC2039.HeatShock.0min.Harvest.70min
+103053  MLC2039.HeatShock.0min.Harvest.180min
+103055  MLC2039.HeatShock.40min.Harvest.70min
+103056  MLC2039.HeatShock.40min.Harvest.180min
+103057  MLC2040.HeatShock.0min.Harvest.70min.control
+103058  MLC2040.HeatShock.0min.Harvest.180min.control
+103059  MLC2040.HeatShock.40min.Harvest.70min.control
+103060  MLC2040.HeatShock.40min.Harvest.180min.control
+[7msampleInfos_parsed.txt (END)[m[K[K[?1l>[2J[?47l8]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m 
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m 
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m 
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m 
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m less sampleInfos_parsed.txt l[KRscript ~/scripts/ngs_tools/parsing_specialCharacters_4designMatrix.R saampleInfos.xlsx 
+libraries imported and start parsing 
+done parsing 
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m qstat
+             JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)
+            167759         c  bowtie2 jingkui.  R      52:38      1 clip-c1-22
+            167760         c  bowtie2 jingkui.  R      52:38      1 clip-c1-22
+            167763         c  bowtie2 jingkui.  R      52:38      1 clip-c1-20
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m qstat
+             JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)
+            167759         c  bowtie2 jingkui.  R      52:39      1 clip-c1-22
+            167760         c  bowtie2 jingkui.  R      52:39      1 clip-c1-22
+            167763         c  bowtie2 jingkui.  R      52:39      1 clip-c1-20
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m qstat
+             JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)
+            167759         c  bowtie2 jingkui.  R      52:40      1 clip-c1-22
+            167760         c  bowtie2 jingkui.  R      52:40      1 clip-c1-22
+            167763         c  bowtie2 jingkui.  R      52:40      1 clip-c1-20
+]0;jingkui.wang@clip-login-0:/groups/cochella/jiwang/Projects/Ariane/R8764_atac[31m$[m[32m/groups/cochella/jiwang/Projects/Ariane/R8764_atac[m[31m:>[m qstat
+             JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)
+            167759         c
